e4e09fc3acba700d0539bc1b532bbc71a157e0d6
gg <piasek.gaba@gmail.com>
2019-11-09 14:55:24 +0100

    add class active to the clicked link

--- js/script.js
+++ js/script.js
@@ -3,17 +3,24 @@
     console.log('links:', links);
   });*/
   const titleClickHandler = function(event){
-    console.log(event);
-    /* remove class 'active' from all article links  */
+    const clickedElement = this;
+    console.log('Link was clicked!');
+
+    /* [DONE] remove class 'active' from all article links  */
+
     const activeLinks = document.querySelectorAll('.titles a.active');
 
     for(let activeLink of activeLinks){
     activeLink.classList.remove('active');
     }
 
-  /* add class 'active' to the clicked link */
+  /* [DONE] add class 'active' to the clicked link */
 
-  /* remove class 'active' from all articles */
+    this .classList.add('active');
+    console.log('clickedElement:', clickedElement);
+
+  /* [DONE] remove class 'active' from all articles */
+
     const activeArticles = document.querySelectorAll('.posts article.active');
 
     for(let _activeArticle of activeArticles){
